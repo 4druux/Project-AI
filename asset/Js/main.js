9f789b5176ec9c4f1cc712c4482e90fdd2ed6e95
andrew <andrewsamagachandra@gmail.com>
2024-06-20 14:54:57 +0700

    update

--- asset/Js/main.js
+++ asset/Js/main.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Fungsi untuk menghitung total penjualan
   function hitungTotal() {
-    const jumlah = parseFloat(jumlahInput.value) || 0;
-    const harga = parseFloat(hargaInput.value) || 0;
+    const jumlah = parseFloat(jumlahInput.value.replace(/\./g, '')) || 0;
+    const harga = parseFloat(hargaInput.value.replace(/\./g, '')) || 0;
     return jumlah * harga;
   }
 
@@ -18,17 +18,41 @@ document.addEventListener("DOMContentLoaded", function () {
     return jumlah < 100 ? "Rendah" : "Tinggi";
   }
 
+  // Fungsi untuk memformat angka ke format ribuan
+  function formatNumber(number) {
+    return number.toLocaleString('id-ID');
+  }
+
+  // Event listener untuk memformat input jumlah
+  jumlahInput.addEventListener('input', function (event) {
+    const value = event.target.value.replace(/\./g, '');
+    if (!isNaN(value) && value !== '') {
+      event.target.value = formatNumber(parseInt(value, 10));
+    } else {
+      event.target.value = '';
+    }
+  });
+
+  // Event listener untuk memformat input harga
+  hargaInput.addEventListener('input', function (event) {
+    const value = event.target.value.replace(/\./g, '');
+    if (!isNaN(value) && value !== '') {
+      event.target.value = formatNumber(parseInt(value, 10));
+    } else {
+      event.target.value = '';
+    }
+  });
+
   // Event listener untuk menentukan kategori ketika formulir dikirim
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const totalPenjualan = hitungTotal();
-    const jumlah = parseFloat(jumlahInput.value) || 0;
+    const jumlah = parseFloat(jumlahInput.value.replace(/\./g, '')) || 0;
     const kategoriPenjualan = tentukanKategori(jumlah);
 
-    totalPenjualanElem.textContent = "Total Penjualan: " + totalPenjualan;
-    kategoriPenjualanElem.textContent =
-      "Kategori Penjualan: " + kategoriPenjualan;
+    totalPenjualanElem.textContent = "Total Penjualan: " + totalPenjualan.toLocaleString('id-ID');
+    kategoriPenjualanElem.textContent = "Kategori Penjualan: " + kategoriPenjualan;
 
     resultDiv.style.display = "block";
   });
